c29c5ae551e51ffb4b72315d663342553d9efaeb
milahu <milahu@gmail.com>
2021-04-17 10:49:26 +0200

    default.nix: start adding libcyaml

--- default.nix
+++ default.nix
@@ -5,6 +5,11 @@ with import <nixpkgs> {};
 let
   #debugFlags = "";
   debugFlags = "-g";
+
+  # https://github.com/nix-community/NUR#how-to-use
+  # error: undefined variable 'nur' at /home/user/src/glib-config-parser/default.nix:11:14
+  ####libcyaml = nur.repos.suhr.libcyaml;
+
 in
 stdenv.mkDerivation {
 
@@ -12,8 +17,9 @@ stdenv.mkDerivation {
   version = "0.0.1";
 
   buildInputs = [
-    glib
-    libyaml
+    glib # util fns, required by spotify anyway (zero cost)
+    libyaml # low level yaml parser
+    ####libcyaml # schema-based yaml parser
   ];
 
   makeFlags  = [
@@ -21,9 +27,15 @@ stdenv.mkDerivation {
     "sysconfdir=${placeholder "out"}/etc" # absolute path to /etc
   ];
 
+/* TODO restore with libcyaml
+    makeFlagsArray+=(CFLAGS="${debugFlags} -I${glib.dev}/include/glib-2.0 -I${glib.out}/lib/glib-2.0/include -I${libyaml}/include -I${libcyaml}/include")
+    makeFlagsArray+=(LDFLAGS="-L${glib.out}/lib -L${libyaml.out}/lib -L${libcyaml.out}/lib")
+*/
+
+  # TODO use pkg-config to generate paths to include and lib
   preBuild = ''
-    makeFlagsArray+=(CFLAGS="${debugFlags} -I${glib.dev}/include/glib-2.0 -I${glib.out}/lib/glib-2.0/include -I${libyaml}/include") # glib.h + glibconfig.h
-    makeFlagsArray+=(LDFLAGS="-L${glib.out}/lib -L${libyaml.out}/lib") # libglib-2.0.so
+    makeFlagsArray+=(CFLAGS="${debugFlags} -I${glib.dev}/include/glib-2.0 -I${glib.out}/lib/glib-2.0/include -I${libyaml}/include")
+    makeFlagsArray+=(LDFLAGS="-L${glib.out}/lib -L${libyaml.out}/lib")
   '';
 
   src = ./src;
